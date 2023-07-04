af7af45c194989ad3a684d60e888a1ed84faf0e2
Mohammed Husamuddin <uni.husam@gmail.com>
2023-07-04 22:45:33 +0400

    Create App

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+    st.title("Rohit Srinivasan's Portfolio")
+    
+    st.header("Coming Soon!")
+    st.markdown("Stay tuned for updates on my portfolio.")
+    
+    st.header("Contact")
+    st.markdown("Feel free to reach out to me:")
+    st.markdown("- Email: rohits@example.com")
+    st.markdown("- LinkedIn: [linkedin.com/in/rohits](https://linkedin.com/in/rohits)")
+    
+if __name__ == "__main__":
+    main()
